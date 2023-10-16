5f682808c96e7ba287b75f07fda1d4aa5a75eace
R-G2110 <argeldelacruz86@gmail.com>
2023-10-16 12:21:05 +0200

    [Init]: App.vue components (header + main)

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name:'Header'
+}
+</script>
+
+<template>
+	<header>
+		<h1>HEADER</h1>
+	</header>
+	
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name:'Main'
+}
+</script>
+
+<template>
+	<main>
+		<h1>MAIN</h1>
+	</main>
+	
+</template>
+
+<style lang="scss" scoped>
+
+</style>
